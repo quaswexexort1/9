6be2f62627b1ec67abfec937e73062be1bf9d914
SHEL <SHEL@DESKTOP-6SFOS1M>
2024-11-10 17:55:01 +0200

    Добавьте файлы проекта.

---
+++ Вычисление бесконечных сумм/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите значение x:");
+double x = double.Parse(Console.ReadLine());
+
+Console.Write("Введите количество членов ряда (n):");
+int n = int.Parse(Console.ReadLine());
+
+double s = 0;
+for (int i = 1; i <= n; i++)
+{
+    double t = Math.Pow(x, 4 * i + 1) / Factorial(4 * i + 1);
+    s += t;
+}
+
+Console.WriteLine($"Сумма ряда: {s}");
+
+
+double Factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
